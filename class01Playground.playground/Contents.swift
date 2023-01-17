e6f4ae2f1f14de0f045e6e696ae25a6907f13548
MNKR2001 <99158889+MNKR2001@users.noreply.github.com>
2023-01-17 09:09:33 -0600

    Add files via upload

---
+++ class01Playground.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+print("Hii,10,12.25");
+var age=23;
+print("My age is \(age) and in the next \(age)years, it would be \(age*2).")
+print("""
+
+Hello
+World
+""")
+//Explicit converstion
+var str:String = "This is a playground ";
+print(str);
+
+print("Hello All\rWelcome to Swift Programing")
+let welcomemessage : String = "Hello!"
+print (welcomemessage,"All");
+
+print("Welcome to swift Programming ")
+print("Fall 2021")
+print("**************")
+print("Welcome to swift programmming",terminator:"$")
+print("Fall 2021")
+
+print
